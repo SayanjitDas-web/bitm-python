a44f25a2585411e3bfb1b4213b20e974b3dd5e6c
SayanjitDas-web <thewebman01@gmail.com>
2026-05-23 20:20:46 +0530

    Refactor countdown timer implementation and remove unused increment function

--- threadingprog.py
+++ threadingprog.py
@@ -17,21 +17,49 @@ import time
 
 # print("all task completed!")
 
-counter = 0
-lock = threading.Lock()
+# counter = 0
+# lock = threading.Lock()
 
-def increment():
-    global counter
-    for _ in range(100000):
-        with lock:
-            counter += 1
+# def increment():
+#     global counter
+#     for _ in range(100000):
+#         with lock:
+#             counter += 1
             
-threads = [threading.Thread(target=increment) for _ in range(2)]
+# threads = [threading.Thread(target=increment) for _ in range(2)]
 
-for t in threads:
-    t.start()
+# for t in threads:
+#     t.start()
     
-for t in threads:
-    t.join()
+# for t in threads:
+#     t.join()
     
-print("final counter: ", counter)
+# print("final counter: ", counter)
+
+# Exercise 1 — Parallel Countdown Timer
+# Goal:
+# Run multiple countdown timers simultaneously using threads.
+# output:
+# Timer A: 5
+# Timer B: 3
+# Timer A: 4
+# Timer B: 2
+
+def countdown(name,sec):
+    while sec > 0:
+        print(f"Timer {name}: {sec}")
+        time.sleep(1)
+        sec -= 1
+        
+    print(f"Timer {name} finished!")
+    
+timer1 = threading.Thread(target=countdown,args=("A",5))
+timer2 = threading.Thread(target=countdown,args=("B",3))
+
+timer1.start()
+timer2.start()
+
+timer1.join()
+timer2.join()
+
+print("all timer finished")

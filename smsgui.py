900bad80a7d08d1ceb5937227d4557fa75874829
SayanjitDas-web <thewebman01@gmail.com>
2026-06-06 20:31:19 +0530

    Add student data display functionality and update GUI layout

--- smsgui.py
+++ smsgui.py
@@ -9,13 +9,27 @@ class StudentManagementSystem:
         with open("records.txt","a") as db:
             db.write(data)
             db.close()
-            
+        display_data_table()
+        name_entry_var.set("")
+        roll_entry_var.set("")
+        year_entry_var.set("")
+        
 sms = StudentManagementSystem()
 
 root = tk.Tk()
 root.title("Student Management System")
 
-root.geometry("600x400")
+root.geometry("700x400")
+
+def display_data_table():
+    with open("records.txt","r") as db:
+        lines = db.readlines()
+        for i in range(0,len(lines)):
+            tk.Label(text=lines[i].split(",")[0]).grid(row=5+i,column=0)
+            tk.Label(text=lines[i].split(",")[1]).grid(row=5+i,column=1)
+            tk.Label(text=lines[i].split(",")[2]).grid(row=5+i,column=2)
+            tk.Label(text=lines[i].split(",")[3]).grid(row=5+i,column=3)
+        db.close()
 
 name_entry_var = tk.StringVar()
 roll_entry_var = tk.StringVar()
@@ -39,5 +53,12 @@ year_entry.grid(row=2,column=1)
 add_atudent_btn = tk.Button(text="Add Student",
         command=lambda: sms.add_data(name_entry_var.get(),roll_entry_var.get(),year_entry_var.get()),
     ).grid(row=3,column=0,columnspan=2)
+
+tk.Label(text="ID").grid(row=4,column=0)
+tk.Label(text="NAME").grid(row=4,column=1)
+tk.Label(text="ROLL").grid(row=4,column=2)
+tk.Label(text="YEAR").grid(row=4,column=3,padx=100)
+
+display_data_table()
 
 root.mainloop()

f79c0e78215f51c7f5276af8dbca2fabeaf5e469
SayanjitDas-web <thewebman01@gmail.com>
2026-06-13 20:25:40 +0530

    Refactor revision.py: clean up commented code and maintain variable declarations

--- revision.py
+++ revision.py
@@ -1,3 +1,20 @@
 my_var = 10
 my_str_var = "Sayanjit"
 my_bool_var = True
+
+# print(my_var)
+# print(100)
+
+# if(10 > 5):
+#     print("Right")
+# elif( 2 < 5):
+#     print("2 is less than 5")
+# else:
+#     print("Wrong")
+    
+# for i in range(5,10):
+#     print(i)
+    
+for char in my_str_var:
+    print(char)
+    
